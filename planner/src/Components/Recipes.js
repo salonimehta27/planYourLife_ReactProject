1fcb49f6e4fe62ac96ded9bae76e7c4b13f0d4ce
Saloni <salonimehta2127@gmail.com>
2021-08-13 01:34:57 -0400

    Created Readme

--- planner/src/Components/Recipes.js
+++ planner/src/Components/Recipes.js
@@ -8,22 +8,26 @@ function Recipes() {
         fetch("https://www.themealdb.com/api/json/v1/1/random.php")
             .then(resp => resp.json())
             .then(data => {
-                // console.log(data.meals.flat())
+                console.log(data.meals.flat())
                 setShowRecipe(data.meals.flat())
 
             })
     }, [])
     const url = showRecipe.map(x => x.strYoutube.split("/"))
-    // console.log(url)
+    console.log(url)
     const cutFirst = url.flat().splice(0, 3)
+    // console.log(cutFirst)
     const joinUrl = cutFirst.join("/")
+    // console.log(joinUrl)
     const cutLast = url.flat().splice(3, 4).toString()
+    // console.log(cutLast)
     const cut = cutLast.slice(8, 20)
+    // console.log(cut)
     const fullUrl = joinUrl + "/" + "embed" + "/" + cut
-
+    // console.log(fullUrl)
     return (
         <div>
-            {showRecipe.map((r) => <RecipeCard key={r.id} r={r} fullUrl={fullUrl} />)}
+            {showRecipe.map((r) => <RecipeCard key={r.idMeal} r={r} fullUrl={fullUrl} />)}
             <Grocery />
         </div>
     )
